5f837483c40c6128bab441d5c6b60d7428cc66c9
olegraikevich <oraikevich@sardinasystems.com>
2024-09-04 08:38:50 +0400

    Update telegram bot logic

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,8 +1,11 @@
 import logging
 import os
-from telegram import Bot
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
+# from telegram import Bot
+# from telegram import Update
+# from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
+from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+
 
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 # bot = Bot(token=bot_token)
@@ -31,40 +34,134 @@ SECOND_MENU_MARKUP = InlineKeyboardMarkup([
 ])
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, 
-        text="Hello!"
+# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await context.bot.send_message(
+#         chat_id=update.effective_chat.id, 
+#         text="Hello!"
+#     )
+
+# async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """
+    This function would be added to the dispatcher as a handler for messages coming from the Bot API
+    """
+
+    # Print to console
+    print(f'{update.message.from_user.first_name} wrote {update.message.text}')
+
+    if screaming and update.message.text:
+        context.bot.send_message(
+            update.message.chat_id,
+            update.message.text.upper(),
+            # To preserve the markdown, we attach entities (bold, italic...)
+            entities=update.message.entities
+        )
+    else:
+        # This is equivalent to forwarding, without the sender's name
+        update.message.copy(update.message.chat_id)
+
+
+def scream(update: Update, context: CallbackContext) -> None:
+    """
+    This function handles the /scream command
+    """
+
+    global screaming
+    screaming = True
+
+
+def whisper(update: Update, context: CallbackContext) -> None:
+    """
+    This function handles /whisper command
+    """
+
+    global screaming
+    screaming = False
+
+
+def menu(update: Update, context: CallbackContext) -> None:
+    """
+    This handler sends a menu with the inline buttons we pre-assigned above
+    """
+
+    context.bot.send_message(
+        update.message.from_user.id,
+        FIRST_MENU,
+        parse_mode=ParseMode.HTML,
+        reply_markup=FIRST_MENU_MARKUP
     )
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=update.message.text
+
+def button_tap(update: Update, context: CallbackContext) -> None:
+    """
+    This handler processes the inline buttons on the menu
+    """
+
+    data = update.callback_query.data
+    text = ''
+    markup = None
+
+    if data == NEXT_BUTTON:
+        text = SECOND_MENU
+        markup = SECOND_MENU_MARKUP
+    elif data == BACK_BUTTON:
+        text = FIRST_MENU
+        markup = FIRST_MENU_MARKUP
+
+    # Close the query to end the client-side loading animation
+    update.callback_query.answer()
+
+    # Update message content with corresponding menu section
+    update.callback_query.message.edit_text(
+        text,
+        ParseMode.HTML,
+        reply_markup=markup
     )
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=text_caps
-    )
 
-async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+def main() -> None:
+    updater = Updater("bot_token")
+
+    # Get the dispatcher to register handlers
+    # Then, we register each handler and the conditions the update must meet to trigger it
+    dispatcher = updater.dispatcher
+
+    # Register commands
+    dispatcher.add_handler(CommandHandler("scream", scream))
+    dispatcher.add_handler(CommandHandler("whisper", whisper))
+    dispatcher.add_handler(CommandHandler("menu", menu))
+
+    # Register handler for inline buttons
+    dispatcher.add_handler(CallbackQueryHandler(button_tap))
+
+    # Echo any message that is not a command
+    dispatcher.add_handler(MessageHandler(~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(bot_token).build()
-    
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    caps_handler = CommandHandler('caps', caps)
+    main()
 
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-    application.add_handler(caps_handler)
-    
-    unknown_handler = MessageHandler(filters.COMMAND, unknown)
-    application.add_handler(unknown_handler)
 
-    application.run_polling()
+
+
+
+# if __name__ == '__main__':
+#     application = ApplicationBuilder().token(bot_token).build()
+    
+#     start_handler = CommandHandler('start', start)
+#     # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+#     application.add_handler(start_handler)
+    
+#     unknown_handler = MessageHandler(filters.COMMAND, unknown)
+#     application.add_handler(unknown_handler)
+
+#     application.run_polling()
